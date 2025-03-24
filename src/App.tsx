0fdbc83db47f0991c564bdb460665c8d48b23854
Serhii Yefimov <sergii.iefimov@gmail.com>
2025-03-24 19:20:39 +0200

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,42 @@
-import React from 'react';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import {
+  HomePage,
+  ProductDetailsPage,
+  FavouritesPage,
+  CartPage,
+  NotFoundPage,
+  ProductsPage,
+} from './pages';
+import { Layout } from './components';
+import { AppRoute, Categories } from './enums';
 import './App.scss';
 
-interface Props {
-  onClick: () => void;
-  children: React.ReactNode;
-}
-
-export const Provider: React.FC<Props> = React.memo(({ onClick, children }) => (
-  <button type="button" onClick={onClick}>
-    {children}
-  </button>
-));
-
-export const App: React.FC = () => {
-  return (
-    <div className="starter">
-      <Provider onClick={() => ({})}>TodoList</Provider>
-    </div>
-  );
-};
+export const App = () => (
+  <Router>
+    <h1 className="visually-hidden">Product Catalog</h1>
+    <Routes>
+      <Route path={AppRoute.HOME} element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route
+          path={AppRoute.PHONES}
+          element={<ProductsPage category={Categories.PHONES} />}
+        />
+        <Route
+          path={AppRoute.TABLETS}
+          element={<ProductsPage category={Categories.TABLETS} />}
+        />
+        <Route
+          path={AppRoute.ACCESSORIES}
+          element={<ProductsPage category={Categories.ACCESSORIES} />}
+        />
+        <Route
+          path={AppRoute.PRODUCT_DETAILS(':category', ':productId')}
+          element={<ProductDetailsPage />}
+        />
+        <Route path={AppRoute.FAVOURITES} element={<FavouritesPage />} />
+        <Route path={AppRoute.CART} element={<CartPage />} />
+        <Route path={AppRoute.NOT_FOUND} element={<NotFoundPage />} />
+      </Route>
+    </Routes>
+  </Router>
+);
